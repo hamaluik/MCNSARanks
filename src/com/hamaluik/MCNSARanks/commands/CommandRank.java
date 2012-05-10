79d8aa895a5f5c8d7cf941136617eb7ab0d5e133
Kwinten Guillaume <kwinno@hotmail.com>
2012-05-10 18:38:01 +0200

    Added the ability to check offline players with /rank name

--- src/com/hamaluik/MCNSARanks/commands/CommandRank.java
+++ src/com/hamaluik/MCNSARanks/commands/CommandRank.java
@@ -1,7 +1,7 @@
 package com.hamaluik.MCNSARanks.commands;
 
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 import ru.tehkode.permissions.PermissionGroup;
 import ru.tehkode.permissions.PermissionUser;
@@ -19,20 +19,37 @@ public class CommandRank implements Command {
 			return false;
 		}
 		
+		//go through all players to find ours
+		OfflinePlayer[] players = plugin.getServer().getOfflinePlayers();
+		int total = players.length;
+		boolean found = false;
+		String player = "";
+		for(int i = 0; i < total; i++) {
+			if(players[i].getName().equalsIgnoreCase(args[0])){
+				found = true;
+				player = players[i].getName();
+			}
+		}
+		//did it find a player?
+		if(found != true){
+			MCNSARanksCommandExecutor.returnMessage(sender, "&cError: player '"+args[0]+"' could not be found!");
+			return true;
+		}
+		/*
 		// query the player
 		Player player = plugin.getServer().getPlayer(args[0]);
 		if(player == null) {
 			MCNSARanksCommandExecutor.returnMessage(sender, "&cError: player '"+args[0]+"' could not be found!");
 			return true;
 		}
-
+		 */
 		PermissionUser user = plugin.permissions.getUser(player);
 		// ok, we have the player.. get their group!
 		PermissionGroup[] groups = user.getGroups();
 		
 		// make sure they have at least 1 group
 		if(groups.length < 1) {
-			MCNSARanksCommandExecutor.returnMessage(sender, "&cError: player '"+player.getName()+"' doesn't have a rank!");
+			MCNSARanksCommandExecutor.returnMessage(sender, "&cError: player '"+player+"' doesn't have a rank!");
 			return true;
 		}
 		
@@ -47,7 +64,7 @@ public class CommandRank implements Command {
 		}
 		
 		// now report it!
-		MCNSARanksCommandExecutor.returnMessage(sender, player.getName() + " is ranked as a: " + group.getPrefix() + group.getName() + "&f!");
+		MCNSARanksCommandExecutor.returnMessage(sender, player + " is ranked as a: " + group.getPrefix() + group.getName() + "&f!");
 		
 		return true;
 	}
